f9a8b0e637c0c1b78a9f8d4b94650b2090a7e70e
omedale <omedale@gmail.com>
2019-06-28 11:44:33 +0100

    fix header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -5,10 +5,18 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 col-sm-12">
-            <a-input-search placeholder="input search text" @search="onSearch" enterButton="Search" :size="'small'" />
+            <router-link :to="'/'" class="home">
+              <h3>My Shop</h3>
+            </router-link>
           </div>
           <div class="col-lg-6 col-sm-12">
             <div class="float-right no-gutters py-1">
+                <a-input-search
+                placeholder="search"
+                class="search-bar"
+                :size="'small'"
+                @search="onSearch"
+              />
               <router-link :to="'/cart'" class="">
                   <span>
                     <a-badge :count="1"><a-avatar shape="square" icon="shopping-cart" /></a-badge>
@@ -41,5 +49,23 @@ export default {
 </script>
 
 <style>
-
+  .search-bar {
+    width: 200px;
+  }
+  .search-bar input {
+    border-radius: 12px;
+  }
+  .ant-avatar {
+    width: 24px !important;
+    height: 24px !important;
+    border-radius: 12px !important;
+    border-radius: 50% !important;
+  }
+  .anticon-shopping-cart {
+    vertical-align: 0.35rem !important;
+    margin-left: -0.10rem  !important;
+  }
+  .home {
+    font-family: 'Merienda', cursive;
+  }
 </style>
